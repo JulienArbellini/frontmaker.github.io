12d0dc4e4dae53f61ac22be5477acc1212c34d68
JulienArbellini <arbellini.j@gmail.com>
2021-08-05 16:51:45 +0200

    responsive navbar

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link href="http://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+  <script src="script.js"></script>
+</head>
+<body>
+  <nav>
+      <div class="logo"><h4>NAVBAR</h4></div>
+      <ul class='nav-links'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">Project</a></li>
+      </ul>
+      <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+  </nav>
+  <main>
+    <div id="top"></div>
+    <div id="parallax1"></div>
+    <div id="mid"></div>
+    <div id="parallax2"></div>
+    <div id="bottom"></div>
+    <div id="parallax3"></div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis porro aliquid sequi labore illo voluptatem, expedita veritatis officiis, velit quidem dolore! Hic neque labore autem veritatis aperiam vel fugit aliquam, illum distinctio error consequuntur inventore aspernatur placeat ab debitis repellendus mollitia nobis molestias, voluptatibus maxime perspiciatis, odit adipisci. Deserunt, aspernatur! Error accusamus doloribus in reprehenderit modi eaque? Optio ad eveniet eius hic? Cumque quibusdam iusto ipsa quo blanditiis dolorem veniam recusandae quasi! Consectetur obcaecati aperiam laborum, dolorem blanditiis provident odit. Recusandae nobis quidem, exercitationem voluptatum rerum cupiditate? Blanditiis, in eos. Libero et voluptates nostrum dignissimos magni saepe beatae commodi dolores.
+  </main>
+  <script src='../js/app.js'></script>
+</body>
+</html>
